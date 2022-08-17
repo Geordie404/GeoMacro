cf77fedc109bb88e185da976e0808d1932fd90c5
Geordie <geordie@MacBook-Pro.local>
2022-08-17 19:31:04 -0400

    user auth

--- Program.cs
+++ Program.cs
@@ -1,4 +1,14 @@
+using Auth0.AspNetCore.Authentication;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// 👇 new code
+builder.Services
+    .AddAuth0WebAppAuthentication(options => {
+      options.Domain = builder.Configuration["Auth0:Domain"];
+      options.ClientId = builder.Configuration["Auth0:ClientId"];
+    });
+// 👆 new code
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -20,6 +30,8 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
+
+app.UseAuthentication(); // 👈 new code
 
 app.MapControllerRoute(
     name: "default",
